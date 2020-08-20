1a6baf9f26038b092c2eccb8ef51721af95611b7
coco <ziyuanli0408@gmail.com>
2020-08-20 15:24:55 -0400

    add redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css'
 
+import { setSearchField, requestRobots } from '../action'
+
+const mapStateToProps = state =>{
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
+	}
+}
+
+const mapDispatchToProps = () => (dispatch) => {
+	return{
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => requestRobots(dispatch)
+	}
+}
+
 
 class App extends React.Component{
-	constructor(){
-		super()
-		this.state = {
-			robots: [],
-			searchfield: ''
-		}
-	}
-
 	componentDidMount(){
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(response => response.json())
-			.then(users => this.setState({ robots: users}));
+		this.props.onRequestRobots();
 	}
-
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value})
-	}
-
 
 	render(){
-		const { robots, searchfield } = this.state
+		const { searchField, onSearchChange, robots, isPending} = this.props;
 		const filterRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		})
 
-		return!robots.length ?
+		return isPending ?
 			<h1> Loading </h1> : 
 			(
 				<div className = 'tc'>
 					<h1 className = 'f1'>RoboFriends</h1>
-					<SearchBox searchChange={this.onSearchChange}/>
+					<SearchBox searchChange={onSearchChange}/>
 					<Scroll>
 						<ErrorBoundry>
 							<CardList robots = {filterRobots} />
@@ -54,4 +58,4 @@ class App extends React.Component{
 	
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
